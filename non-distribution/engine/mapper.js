cd4501377bde016b7f812646fd8a6552465da833
Joseph Dodson <joseph_dodson@brown.edu>
2025-04-05 13:42:01 -0400

    update mapper to use async

--- non-distribution/engine/mapper.js
+++ non-distribution/engine/mapper.js
@@ -3,60 +3,61 @@ const {convert} = require('html-to-text');
 const fs = require('fs');
 const natural = require('natural');
 
-function mapperFunction(key, value) {
+async function mapperFunction(key, value) {
     // key is URL, <nothing -- unicorns>
-    
-    // fetch the page content
-    fetch(key, {method: "GET"}).then(response => {
-        //extract text from HTML
-        response.text().then(html => {
-            // convert the html to text
-            text = convert(html, {wordwrap: 130});
+    try {
+        // fetch the page content
+        const response = await fetch(key, { method: "GET" });
+        const html = await response.text();
+        let text = convert(html, {wordwrap: 130});
 
-            // Process the text to make each word on a newline, remove stopwords, lowercase, etc.
-            
-            // RegEx's
-            // https://www.regular-expressions.info/posixbrackets.html
-            const newLineRegex = /[\p{P} ]/gu;
-            const nonAlphabeticRegex = /[^a-zA-z\n]/g;
-            const notAsciiRegex = /[^\x00-\x7F]/g;
+        // Process the text to make each word on a newline, remove stopwords, lowercase, etc.
+        
+        // RegEx's
+        // https://www.regular-expressions.info/posixbrackets.html
+        const newLineRegex = /[\p{P} ]/gu;
+        const nonAlphabeticRegex = /[^a-zA-z\n]/g;
+        const notAsciiRegex = /[^\x00-\x7F]/g;
 
-            // Read in the stopword corpora
-            const STOPWORDS_PATH = '../non-distribution/d/stopwords.txt';
-            let stopwords = '';
-            try {
-                const data = fs.readFileSync(STOPWORDS_PATH, {encoding: 'utf8', flag: 'r'}).toString();
-                stopwords = data.split('\n');
-            } catch (err) {
-                console.err('Error reading stopwords.txt:', err);
-                return;
-            }
+        // Read in the stopword corpora
+        const STOPWORDS_PATH = '../non-distribution/d/stopwords.txt';
+        let stopwords = '';
+        try {
+            const data = fs.readFileSync(STOPWORDS_PATH, {encoding: 'utf8', flag: 'r'}).toString();
+            stopwords = data.split('\n');
+        } catch (err) {
+            console.err('Error reading stopwords.txt:', err);
+            return;
+        }
 
-            text.replaceAll(newLineRegex, '\n')
-                .replaceAll(nonAlphabeticRegex, '')
-                .toLowerCase()
-                .replaceAll(notAsciiRegex, '')
-                .split('\n')
-                .filter((word) => !(word in stopwords))
-                .filter((word) => word !== "")
-                .join('\n');
+        text.replaceAll(newLineRegex, '\n')
+            .replaceAll(nonAlphabeticRegex, '')
+            .toLowerCase()
+            .replaceAll(notAsciiRegex, '')
+            .split('\n')
+            .filter((word) => !(word in stopwords))
+            .filter((word) => word !== "")
+            .join('\n');
 
 
-            // After processing all the text, then stem everything
-            const stemmer = natural.PorterStemmer;
-            stemmedText = stemmer.stem(text);
-            
-            // Split stemmed text into array --> Combine this array
-            ngrams = generateNgrams(stemmedText.split('\n'));
-            results = [];
-            for (const gram of ngrams) {
-                const out = {};
-                out[gram[0]] = [key, gram[1]];
-                results.push(out);
-            }
-            return results;
-        });
-    })
+        // After processing all the text, then stem everything
+        const stemmer = natural.PorterStemmer;
+        stemmedText = stemmer.stem(text);
+        
+        // Split stemmed text into array --> Combine this array
+        ngrams = generateNgrams(stemmedText.split('\n'));
+        results = [];
+        for (const gram of ngrams) {
+            const out = {};
+            out[gram[0]] = [key, gram[1]];
+            results.push(out);
+        }
+        return results;
+    }
+    catch (err) {
+        console.error('Error in mapper function', err);
+        return [];
+    }
 }
 
 // Combine
